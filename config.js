b1457eca776292f6861b4529fed50f35cef8dc73
Manjunath Reddy <5035624+manjufy@users.noreply.github.com>
2019-05-29 23:39:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const env = process.env.NODE_ENV || 'development';
 const dbConnection = (env) => ({
     port: 3306,
     host: '127.0.0.1',
-    user: 'root',
-    password: 'manju2018',
+    user: 'dbUsername',
+    password: 'dbPassword',
     database: `kljs-e2e-${env}`, // kljs-e2e-{dev|test|prod}
     multipleStatements: true,
     timezone: 'utc',
